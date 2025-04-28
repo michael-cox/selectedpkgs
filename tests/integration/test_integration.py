1f992d6995942b958c67c8bac2fa41d4bc8fac6d
michael-cox <2.cox.michael@gmail.com>
2025-04-28 14:54:12 -0500

    reformat test with black

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -4,12 +4,16 @@ from pathlib import Path
 from selectedpkgs.main import main
 
 dpkg_status = Path("/var/lib/dpkg/status")
-packages = ['vim', 'curl', 'cowsay', 'tmux', 'unzip']
+packages = ["vim", "curl", "cowsay", "tmux", "unzip"]
+
 
 @pytest.fixture(scope="module")
 def install_packages():
-    subprocess.run(['apt', 'update'], check=True)
-    subprocess.run(['apt', 'install', '-y'] + packages, capture_output=True, text=True, check=True)
+    subprocess.run(["apt", "update"], check=True)
+    subprocess.run(
+        ["apt", "install", "-y"] + packages, capture_output=True, text=True, check=True
+    )
+
 
 @pytest.mark.skipif(not dpkg_status.is_file, reason="Requires Debian")
 def test_selectedpkgs(install_packages, capsys):
