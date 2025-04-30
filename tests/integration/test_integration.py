30fcd401bcfa8e07149fa49a70901fc20fd5fac4
michael-cox <2.cox.michael@gmail.com>
2025-04-29 19:25:33 -0500

    implement dependent manual install test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -5,6 +5,7 @@ from selectedpkgs.main import main
 
 dpkg_status = Path("/var/lib/dpkg/status")
 packages = ["vim", "curl", "cowsay", "tmux", "unzip"]
+dependent_pkgs = ["lm-sensors", "htop"]
 
 
 # install packages
@@ -12,14 +13,21 @@ packages = ["vim", "curl", "cowsay", "tmux", "unzip"]
 def install_packages():
     subprocess.run(["apt", "update"], check=True)
     subprocess.run(
-        ["apt", "install", "-y"] + packages, capture_output=True, text=True, check=True
+        ["apt", "install", "-y"] + packages + dependent_pkgs, capture_output=True, text=True, check=True
     )
 
 
 # test that selectedpkgs finds installed packages
 @pytest.mark.skipif(not dpkg_status.is_file, reason="Requires Debian")
-def test_selectedpkgs(install_packages, capsys):
+def test_regular_pkgs(install_packages, capsys):
     main(args=[])
     captured = capsys.readouterr()
     for pkg in packages:
+        assert pkg in captured.out
+
+@pytest.mark.skipif(not dpkg_status.is_file, reason="Requires Debian")
+def test_deep_dependencies(install_packages, capsys):
+    main(args=[])
+    captured = capsys.readouterr()
+    for pkg in dependent_pkgs:
         assert pkg in captured.out
